54a81f92baa456183afb4759ee1f4e45db886bf2
makario85 <50327988+makario85@users.noreply.github.com>
2019-05-06 15:41:03 +0200

    Add files via upload

---
+++ WasteCollectionManagement.Logic/Services/ContainerReadyService.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using WasteCollectionManagement.Model.Models;
+using System.Data.Entity;
+
+namespace WasteCollectionManagement.Logic.Services
+{
+    class CollectionReadyService
+    {
+        private DbSet<Bin> Repository { get; set; }
+
+        public ICollection<Bin> GetAll()
+        {
+            return Repository
+                .Where(p => p.CollectionReady)
+                .Select(x => x)
+                .ToList();
+        }
+    }
+}
---
+++ WasteCollectionManagement.Logic/Services/GPSLocationService.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using WasteCollectionManagement.Model.Models;
+using System.Data.Entity;
+
+namespace WasteCollectionManagement.Logic.Services
+{
+    public class GPSLocationService
+    {
+        private DbSet<GPSLocation> Repository { get; set; }
+
+        public ICollection<GPSLocation> GetAll()
+        {
+            return Repository
+                .Select(x => x)
+                .Include(p => p.Bins)
+                .ToList();
+        }
+    }
+}
